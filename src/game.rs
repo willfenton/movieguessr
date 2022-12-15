1b02b4d1f068fd9178bac75e196ce9a3b31797b0
Will Fenton <wfenton@protonmail.com>
2022-12-15 16:32:47 -0700

    Crossterm testing

--- src/game.rs
+++ src/game.rs
@@ -1,7 +1,8 @@
 use crate::movie::Movie;
 use crate::movie_manager::MovieManager;
-use crossterm::cursor::MoveTo;
+use crossterm::cursor::{MoveRight, MoveTo, MoveToNextLine};
 use crossterm::execute;
+use crossterm::style::{Attribute, Print, SetAttribute};
 use crossterm::terminal::Clear;
 use crossterm::terminal::ClearType::All;
 use std::io::{stdout, Error};
@@ -23,8 +24,38 @@ impl Game {
         // reset
         execute!(stdout(), Clear(All), MoveTo(0, 0))?;
 
-        movie.print();
-        sleep(Duration::from_secs(1));
+        execute!(
+            stdout(),
+            MoveToNextLine(1),
+            MoveRight(1),
+            Print(format!("{} ({})", movie.tmdb.title, movie.omdb.year)),
+            SetAttribute(Attribute::Reset)
+        )?;
+        execute!(
+            stdout(),
+            MoveToNextLine(1),
+            MoveRight(1),
+            Print(format!("Directed by {}", movie.omdb.director)),
+            SetAttribute(Attribute::Reset)
+        )?;
+        execute!(
+            stdout(),
+            MoveToNextLine(1),
+            MoveRight(1),
+            Print(format!("Featuring {}", movie.omdb.actors)),
+            SetAttribute(Attribute::Reset)
+        )?;
+
+        execute!(stdout(), MoveToNextLine(2), Print(movie.omdb.plot), SetAttribute(Attribute::Reset))?;
+        if movie.tmdb.overview.is_some() {
+            execute!(stdout(), MoveToNextLine(2), Print(movie.tmdb.overview.unwrap()), SetAttribute(Attribute::Reset))?;
+        }
+        if movie.tmdb.tagline.is_some() {
+            execute!(stdout(), MoveToNextLine(2), Print(movie.tmdb.tagline.unwrap()), SetAttribute(Attribute::Reset))?;
+        }
+
+        // movie.print();
+        sleep(Duration::from_secs(10));
 
         Ok(())
     }
